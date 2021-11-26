492f82cb9dfccdf8c612730b251c45b18ff67b40
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-11-26 13:51:32 +1300

    added husky

--- src/bl/createGame.test.ts
+++ src/bl/createGame.test.ts
@@ -1,57 +1,57 @@
-import { createGame } from "./createGame";
+import { createGame } from './createGame'
 
-describe("createGame", () => {
-  test("1 x 1", () => {
-    let expected = [[{ rank: "a", file: 1, playerA: {}, playerB: {} }]];
-    let actual = createGame(1, 1);
+describe('createGame', () => {
+   test('1 x 1', () => {
+      let expected = [[{ rank: 'a', file: 1, playerA: {}, playerB: {} }]]
+      let actual = createGame(1, 1)
 
-    expect(actual).toStrictEqual(expected);
-  });
-  test("2 x 1", () => {
-    let expected = [
-      [
-        { rank: "a", file: 1, playerA: {}, playerB: {} },
-        { rank: "b", file: 1, playerA: {}, playerB: {} },
-      ],
-    ];
-    let actual = createGame(2, 1);
+      expect(actual).toStrictEqual(expected)
+   })
+   test('2 x 1', () => {
+      let expected = [
+         [
+            { rank: 'a', file: 1, playerA: {}, playerB: {} },
+            { rank: 'b', file: 1, playerA: {}, playerB: {} },
+         ],
+      ]
+      let actual = createGame(2, 1)
 
-    expect(actual).toStrictEqual(expected);
-  });
+      expect(actual).toStrictEqual(expected)
+   })
 
-  test("2 x 2", () => {
-    let expected = [
-      [
-        { rank: "a", file: 1, playerA: {}, playerB: {} },
-        { rank: "b", file: 1, playerA: {}, playerB: {} },
-      ],
-      [
-        { rank: "a", file: 2, playerA: {}, playerB: {} },
-        { rank: "b", file: 2, playerA: {}, playerB: {} },
-      ],
-    ];
-    let actual = createGame(2, 2);
+   test('2 x 2', () => {
+      let expected = [
+         [
+            { rank: 'a', file: 1, playerA: {}, playerB: {} },
+            { rank: 'b', file: 1, playerA: {}, playerB: {} },
+         ],
+         [
+            { rank: 'a', file: 2, playerA: {}, playerB: {} },
+            { rank: 'b', file: 2, playerA: {}, playerB: {} },
+         ],
+      ]
+      let actual = createGame(2, 2)
 
-    expect(actual).toStrictEqual(expected);
-  });
+      expect(actual).toStrictEqual(expected)
+   })
 
-  test("2 x 3", () => {
-    let expected = [
-      [
-        { rank: "a", file: 1, playerA: {}, playerB: {} },
-        { rank: "b", file: 1, playerA: {}, playerB: {} },
-      ],
-      [
-        { rank: "a", file: 2, playerA: {}, playerB: {} },
-        { rank: "b", file: 2, playerA: {}, playerB: {} },
-      ],
-      [
-        { rank: "a", file: 3, playerA: {}, playerB: {} },
-        { rank: "b", file: 3, playerA: {}, playerB: {} },
-      ],
-    ];
-    let actual = createGame(2, 3);
+   test('2 x 3', () => {
+      let expected = [
+         [
+            { rank: 'a', file: 1, playerA: {}, playerB: {} },
+            { rank: 'b', file: 1, playerA: {}, playerB: {} },
+         ],
+         [
+            { rank: 'a', file: 2, playerA: {}, playerB: {} },
+            { rank: 'b', file: 2, playerA: {}, playerB: {} },
+         ],
+         [
+            { rank: 'a', file: 3, playerA: {}, playerB: {} },
+            { rank: 'b', file: 3, playerA: {}, playerB: {} },
+         ],
+      ]
+      let actual = createGame(2, 3)
 
-    expect(actual).toStrictEqual(expected);
-  });
-});
+      expect(actual).toStrictEqual(expected)
+   })
+})
--- src/bl/createGame.ts
+++ src/bl/createGame.ts
@@ -1,59 +1,59 @@
-import * as R from "ramda";
+import * as R from 'ramda'
 
-type Rank = string;
-type File = number;
+type Rank = string
+type File = number
 
 interface PlayerCellView {
-  occupiedByMe: false;
-  occupiedByThem: false;
-  bombedByMe: false;
-  bombedByThem: false;
+   occupiedByMe: false
+   occupiedByThem: false
+   bombedByMe: false
+   bombedByThem: false
 }
 
 interface Cell {
-  rank: Rank;
-  file: File;
-  playerA: PlayerCellView | {};
-  playerB: PlayerCellView | {};
+   rank: Rank
+   file: File
+   playerA: PlayerCellView | {}
+   playerB: PlayerCellView | {}
 }
 
 interface Game {
-  game: Cell[][];
+   game: Cell[][]
 }
 
 const game = [
-  [
-    {
-      rank: "a",
-      file: 1,
-      playerA: {},
-      playerB: {},
-    },
-  ],
-];
+   [
+      {
+         rank: 'a',
+         file: 1,
+         playerA: {},
+         playerB: {},
+      },
+   ],
+]
 
 enum PLAYER_ID {
-  A = "A",
-  B = "B",
+   A = 'A',
+   B = 'B',
 }
 
 export default {
-  createGame,
-};
+   createGame,
+}
 
 function createCell(rankNum: number, fileNum: number): Cell {
-  return {
-    rank: String.fromCharCode(97 + rankNum - 1) as string,
-    file: fileNum,
-    playerA: {},
-    playerB: {},
-  };
+   return {
+      rank: String.fromCharCode(97 + rankNum - 1) as string,
+      file: fileNum,
+      playerA: {},
+      playerB: {},
+   }
 }
 
 export function createGame(rankNum: number, fileNum: number): Game {
-  return R.range(1, fileNum + 1).map((fileCursor) => {
-    return R.range(1, rankNum + 1).map((rankCursor) => {
-      return createCell(rankCursor, fileCursor);
-    });
-  });
+   return R.range(1, fileNum + 1).map((fileCursor) => {
+      return R.range(1, rankNum + 1).map((rankCursor) => {
+         return createCell(rankCursor, fileCursor)
+      })
+   })
 }
