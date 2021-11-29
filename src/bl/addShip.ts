8406407eadd80555002182cc34f7595e33f7a529
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-11-30 10:35:20 +1300

    tidying

--- src/bl/addShip.ts
+++
@@ -1,6 +0,0 @@
-import { Board, PLAYER_ID, Position } from './game.model'
-
-export function addShip(player: PLAYER_ID, pos: Position, board: Board): Board {
-   board[pos.rank - 1][pos.file - 1].players[player].occupiedByMe = true
-   return board
-}
--- src/bl/engine.test.ts
+++ src/bl/engine.test.ts
@@ -1,37 +1,20 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit'
-
-import { createBoard } from './board'
+import { configureStore } from '@reduxjs/toolkit'
+import { boardSlice } from './engine'
 import { PLAYER_ID } from './game.model'
 
-const counterSlice = createSlice({
-   name: 'counter',
-   initialState: {
-      value: 0,
-      board: createBoard(4, 4),
-   },
-   reducers: {
-      addShip: (state, props) => {
-         const {
-            player,
-            position: { rank, file },
-         } = props.payload
-         state.board[rank][file].players[player].occupiedByMe = true
-      },
-   },
-})
+export const { addShip } = boardSlice.actions
 
-export const { addShip } = counterSlice.actions
+describe('engine', () => {
+   test('addShip', () => {
+      const store = configureStore({
+         reducer: boardSlice.reducer,
+      })
 
-const store = configureStore({
-   reducer: counterSlice.reducer,
-})
-
-// store.subscribe(() => console.log(JSON.stringify(store.getState(), null, 4)))
-
-test('engine', () => {
-   store.dispatch(
-      addShip({ player: PLAYER_ID.A, position: { rank: 1, file: 1 } })
-   )
-   let actual = store.getState().board[1][1].players[PLAYER_ID.A].occupiedByMe
-   expect(actual).toBe(true)
+      store.dispatch(
+         addShip({ player: PLAYER_ID.A, position: { rank: 1, file: 1 } })
+      )
+      let actual =
+         store.getState().board[1][1].players[PLAYER_ID.A].occupiedByMe
+      expect(actual).toBe(true)
+   })
 })
